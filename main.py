7c366a1c77a6610bcb8add40b333f3ae38e92c1b
Anes <suhonjicanes96@gmail.com>
2023-03-27 12:51:35 -0500

    Encrypt Data

--- main.py
+++ main.py
@@ -7,9 +7,22 @@ with open("mykey.key","wb") as mykey:
 				mykey.write(key)
 
 #Load key
-
 with open("mykey.key","rb") as mykey:
 				key = mykey.read()
+
+#Initialize Fernet object
+f = Fernet(key)
+
+#Open data file in plaintext
+with open("data.csv","rb") as file:
+				data = file.read()
+
+# Encrypt data and store as encrypted
+encrypted = f.encrypt(data)
+
+#Write ecrypted data into new csv file
+with open("encrypted_data","wb") as file:
+				file.write(encrypted)
 
 
 
