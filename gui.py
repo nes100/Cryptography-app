6d1118ec75edf874256aa88a277f57ba1baa243e
Anes <suhonjicanes96@gmail.com>
2023-03-27 22:07:01 -0500

    Add functional file browsing buttons

--- gui.py
+++ gui.py
@@ -2,16 +2,27 @@ import cryptographic_functions as cf
 import PySimpleGUI as sg
 import csv, os
 
+working_directory = os.getcwd()
+file_types = (
+    ("All CSV and TXT Files","*.csv *txt"),
+)
+
 label = sg.Text("Encrypt or Decrypt a File")
-input_box1 = sg.Text("Enter the path of the file you want to encrypt"), sg.InputText("FILE_PATH"), \
-				sg.Button("Browse"), sg.Button("Encrypt")
-input_box2 = sg.Text("Enter the path of the file you want to decrypt"), sg.InputText(), \
-				sg.Button("Browse"), sg.Button("Decrypt")
+encrypt =\
+				sg.Text("Enter the path of the file you want to encrypt"),\
+				sg.InputText(),\
+				sg.FileBrowse(initial_folder=working_directory, file_types=file_types),\
+				sg.Button("Encrypt")
+decrypt =\
+				sg.Text("Enter the path of the file you want to decrypt"),\
+				sg.InputText(),\
+				sg.FileBrowse(initial_folder=working_directory, file_types=file_types),\
+				sg.Button("Decrypt")
 
 window = sg.Window("Cryptography App",
 																			layout=[[label],
-																										[input_box1],
-																											[input_box2]],
+																										[encrypt],
+																									 [decrypt]],
 																			font=("Helvetica",14))
 
 window.read()
